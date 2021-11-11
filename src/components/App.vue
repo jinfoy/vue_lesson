edf687d6d54bca2fe4d937d0d1979cc6423e66bb
北島大志 <>
2021-11-11 18:26:15 +0900

    add new file

---
+++ src/components/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h1
+      v-html="message"
+      v-bind:class="classObject"
+    ></h1>
+    <hr>
+    <child-component
+      v-show="isShow"
+    >
+      <template #head>
+        <p>head slot</p>
+      </template>
+      <template #default>
+        <p>main slot</p>
+        <p>main slot2</p>
+      </template>
+      <template #foot>
+        <p>foot slot</p>
+      </template>
+    </child-component>
+    <hr>
+    <p v-if="id === 1">1</p>
+    <template v-else-if="id === 2">
+      <p>2-1</p>
+      <p>2-2</p>
+      <p>2-3</p>
+    </template>
+    <p v-else>other</p>
+    <hr>
+    <template v-for="item in items">
+      <child-component
+        :key="item.id"
+        :title="item.title"
+      >
+        <span>slot content</span>
+      </child-component>
+    </template>
+    <hr>
+    <button @click="incrementCount">Add to count</button>
+    <p>{{ count }}回クリックしました</p>
+    <hr>
+    <input type="text" v-model="inputText">
+    <p>computed: {{ getUpperCaseText }}</p>
+    <p>methods: {{ showUpperCaseText() }}</p>
+  </div>
+</template>
+
+<script>
+import ChildComponent from 'Components/ChildComponent';
+
+export default {
+  //es6のメソッド記法
+  data() {
+    return {
+      message: '<span>Hello Vue!</span>',
+      isShow: true,
+      id: 2,
+      count: 0,
+      inputText: '',
+      classObject: {
+        'is-green': true,
+      },
+      items: [
+        {
+          id: this.$uuid.v4(),
+          title: '1番のリスト',
+        },
+        {
+          id: this.$uuid.v4(),
+          title: '2番目のリスト',
+        },
+        {
+          id: this.$uuid.v4(),
+          title: '3番目のリスト',
+        }
+      ]
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    },
+    showUpperCaseText() {
+      const upperCaseText = this.inputText.toUpperCase();
+      console.log('method: ${upperCaseText}');
+      return upperCaseText;
+    }
+  },
+  computed: {
+    getUpperCaseText() {
+      const UpperCaseText = this.inputText.toUpperCase();
+      console.log('computed: ${UpperCaseText}');
+      return UpperCaseText;
+    }
+  },
+  components: {
+    ChildComponent,
+  }
+}
+</script>
+
+<style scoped>
+  .is-green {
+    color: green;
+  }
+  hr {
+    margin: 16px 0;
+  }
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <slot name="head"></slot>
+    <slot>default slot</slot>
+    <slot name="foot"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    }
+  }
+}
+</script>
